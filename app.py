08d2e2cbca8ad87e6ea958d5fb9c5a55a8b19a1a
jivandabeast <jivanramjisingh@gmail.com>
2025-09-11 19:49:34 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+Competitive Ranked Wordle Discord Bot
+    A Discord bot application to handle user submissions and reporting
+
+Copyright (C) 2025  Jivan RamjiSingh
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
